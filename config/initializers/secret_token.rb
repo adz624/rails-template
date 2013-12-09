c2f18b7ddc7044ad118018c507179128e13ca856
eddie <eddie@visionbundles.com>
2013-12-09 15:23:31 +0800

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Visionbundles::Application.config.secret_key_base = '1794cd660ab7bbe231b55b3a7a2429557dee6ad8f7ef40a8d67e6d40ab9408ee6fa591f4be6346ed1381f2cc5e6c50084cf0aeb7d018971bae5594c9706ae039'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Visionbundles::Application.config.session_store :cookie_store, key: '_visionbundles_session'
